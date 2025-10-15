c10727725f12cc6262561f55fa24ac24ffe0ad26
𝑫𝑽𝑴𝑩 <infentronthegamer@gmail.com>
2025-10-15 15:58:20 +0530

    Update Kya.py

--- Kya.py
+++ Kya.py
@@ -4,10 +4,10 @@ tokenbot = os.getenv('kya')
 channel = -1002162858751 
  
 
-url = f"https://api.telegram.org/bot{tokenbot}/getChatMember?chat_id={channel}&user_id={teleid}"
-response = requests.get(url).json()
+dvmb = f"https://api.telegram.org/bot{tokenbot}/getChatMember?chat_id={channel}&user_id={teleid}"
+response = requests.get(dvmb).json()
 
 if not (response.get("ok") and response.get("result", {}).get("status") in ["member", "administrator", "creator"]):
-    anim(f""" ㅤ{black}ㅤ\033[103m[ 🚨 ]    𝐉𝚘𝚒𝚗 𝐀𝚕𝚕 𝐂𝚑𝚊𝚗𝚗𝚎𝚕𝚜 𝐓𝚑𝚎𝚗 𝐎𝚗𝚕𝚢 𝐘𝚘𝚞 𝐂𝚊𝚗 𝐏𝚛𝚘𝚌𝚎𝚎𝚍.{reset}""")
+    anim(f""" ㅤ{black}ㅤ\033[101m[ 🚨 ]    𝐉𝚘𝚒𝚗 𝐀𝚕𝚕 𝐂𝚑𝚊𝚗𝚗𝚎𝚕𝚜 𝐓𝚑𝚎𝚗 𝐎𝚗𝚕𝚢 𝐘𝚘𝚞 𝐂𝚊𝚗 𝐏𝚛𝚘𝚌𝚎𝚎𝚍.{reset}""")
     webbrowser.open("https://t.me/addlist/-zdOU4i16nNjZjll")
     sys.exit()
