aa5d7ff4ac908d7f76f7d47bb947e86b6a80fb92
𝑫𝑽𝑴𝑩 <infentronthegamer@gmail.com>
2025-10-15 15:15:10 +0530

    Create Kya.py

---
+++ Kya.py
@@ -0,0 +1,15 @@
+import webbrowser, time, requests, sys, os
+
+tokenbot = os.getenv('KYA')
+channel = "@unsely" 
+time.sleep(1) 
+
+url = f"https://api.telegram.org/bot{tokenbot}/getChatMember?chat_id={channel}&user_id={teleid}"
+response = requests.get(url).json()
+
+print("\r", end="", flush=True)
+
+if not (response.get("ok") and response.get("result", {}).get("status") in ["member", "administrator", "creator"]):
+    print(" ㅤ⚚  𝐍ɪɢɢᴇʀ 𝐉ᴏɪɴ 𝐓ʜᴇ 𝐂ʜᴀɴɴᴇʟ 𝐓ᴏ 𝐔𝚂ᴇ 𝐓ʜᴇ 𝐓ᴏᴏʟ")
+    sys.exit()
+    
