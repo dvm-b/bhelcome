aef1af4ed765fe6cd8a1bdf1936f03d7449f30c2
𝑫𝑽𝑴𝑩 <infentronthegamer@gmail.com>
2025-10-15 19:53:08 +0530

    Update Kya.py

--- Kya.py
+++ Kya.py
@@ -10,6 +10,5 @@ response = requests.get(dvmb).json()
 if not (response.get("ok") and response.get("result", {}).get("status") in ["member", "administrator", "creator"]):
     anim(f""" ㅤ{black}ㅤ\033[101m[ 🚨 ]    𝐉𝚘𝚒𝚗 𝐀𝚕𝚕 𝐂𝚑𝚊𝚗𝚗𝚎𝚕𝚜 𝐓𝚑𝚎𝚗 𝐎𝚗𝚕𝚢 𝐘𝚘𝚞 𝐂𝚊𝚗 𝐏𝚛𝚘𝚌𝚎𝚎𝚍.{reset}""")
     webbrowser.open("https://t.me/addlist/-zdOU4i16nNjZjll")
-except Exception as e :
-    print(f"{e}")
+    
     sys.exit()
