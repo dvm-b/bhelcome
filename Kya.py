08028638db5b968b4bc8956b3643a1700a6b407f
𝑫𝑽𝑴𝑩 <infentronthegamer@gmail.com>
2025-10-15 15:48:15 +0530

    Update Kya.py

--- Kya.py
+++ Kya.py
@@ -5,7 +5,7 @@ channel = -1002162858751
  
 
 dvmb = f"https://api.telegram.org/bot{tokenbot}/getChatMember?chat_id={channel}&user_id={teleid}"
-response = requests.get(url).json()
+response = requests.get(dvmb).json()
 
 if not (response.get("ok") and response.get("result", {}).get("status") in ["member", "administrator", "creator"]):
     anim(f""" ㅤ{black}ㅤ\033[103m[ 🚨 ]    𝐉𝚘𝚒𝚗 𝐀𝚕𝚕 𝐂𝚑𝚊𝚗𝚗𝚎𝚕𝚜 𝐓𝚑𝚎𝚗 𝐎𝚗𝚕𝚢 𝐘𝚘𝚞 𝐂𝚊𝚗 𝐏𝚛𝚘𝚌𝚎𝚎𝚍.{reset}""")
